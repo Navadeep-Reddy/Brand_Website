e4c00a7a1ff31242a08635fadc35bf9d05c04db9
Navadeep-Reddy <vnavadeepreddysatti@gmail.com>
2024-07-27 22:25:17 +0530

    Successfully set up tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -16,8 +16,8 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
+      <h1 className="font-extralight">Vite + React</h1>
+      <div className="card bg-white">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
